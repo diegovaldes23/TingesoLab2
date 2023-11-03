8227b3234714074e8725f898c43c1879f6e6f6cd
diegovaldes23 <diego.valdes.f@usach.cl>
2023-11-03 18:07:26 -0300

    front

--- tingeso2/estudiante-service/src/main/java/com/example/estudianteservice/EstudianteServiceApplication.java
+++ tingeso2/estudiante-service/src/main/java/com/example/estudianteservice/EstudianteServiceApplication.java
@@ -2,8 +2,9 @@ package com.example.estudianteservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
+@EnableEurekaClient
 public class EstudianteServiceApplication {
 
 	public static void main(String[] args) {
--- tingeso2/resumen-service/src/main/java/com/example/resumenservice/ResumenServiceApplication.java
+++ tingeso2/resumen-service/src/main/java/com/example/resumenservice/ResumenServiceApplication.java
@@ -2,8 +2,11 @@ package com.example.resumenservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+
 public class ResumenServiceApplication {
 
 	public static void main(String[] args) {
