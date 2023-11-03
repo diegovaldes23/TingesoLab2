e1e52f8980528fd62d6fc295f4b5f9aa1e12dbd8
diegovaldes23 <diego.valdes.f@usach.cl>
2023-11-03 16:39:00 -0300

    ymls

--- tingeso2/config-service/src/test/java/com/example/configservice/ConfigServiceApplicationTests.java
+++ tingeso2/config-service/src/test/java/com/example/configservice/ConfigServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.tingeso.gatewayservice;
+package com.example.configservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayServiceApplicationTests {
+class ConfigServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
--- tingeso2/gateway-service/src/test/java/com/example/gatewayservice/GatewayServiceApplicationTests.java
+++ tingeso2/gateway-service/src/test/java/com/example/gatewayservice/GatewayServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.tingeso.configservice;
+package com.example.gatewayservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigServiceApplicationTests {
+class GatewayServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
