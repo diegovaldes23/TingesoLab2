292829c13670d04b1b84d7f64e4d39b131ac5dba
diegovaldes23 <diego.valdes.f@usach.cl>
2023-11-11 12:39:02 -0300

    .

--- tingeso2/resumen-service/src/main/java/com/example/resumenservice/ResumenServiceApplication.java
+++ tingeso2/resumen-service/src/main/java/com/example/resumenservice/ResumenServiceApplication.java
@@ -3,14 +3,13 @@ package com.example.resumenservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 
 public class ResumenServiceApplication {
 
